e1448452fb924e879cd8a0dd9075706b25948a5c
BrunoC <corraobruno13@gmail.com>
2023-08-15 18:43:02 -0300

    added cors dependecy and middleware

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,13 @@ import connectDB from "./config/db.js";
 const port = process.env.PORT || 5000;
 import userRoutes from "./routes/userRoutes.js";
 import invoiceRoutes from "./routes/invoiceRoutes.js";
+import cors from 'cors'
 
 connectDB();
 
 const app = express();
+
+app.use(cors)
 
 app.use(express.json());
 
