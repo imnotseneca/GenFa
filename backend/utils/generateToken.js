149766534fcbaccad373badc052ebd92da83f7d1
BrunoC <corraobruno13@gmail.com>
2023-08-15 20:04:48 -0300

    modified cookie making on generateToken

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -5,10 +5,11 @@ const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "30d",
   });
+  console.log(token)
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
+    secure: process.env.NODE_ENV === "production",
     sameSite: "strict",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
