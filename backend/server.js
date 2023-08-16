4d1af76996f3eda220d420c6b67a5dcb22987600
BrunoC <corraobruno13@gmail.com>
2023-08-16 11:17:56 -0300

    trying to fix cookie issues

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { path } from "path";
+import path from "path";
 import dotenv from "dotenv";
 dotenv.config();
 import cookieParser from "cookie-parser";
@@ -8,15 +8,17 @@ import connectDB from "./config/db.js";
 const port = process.env.PORT || 5000;
 import userRoutes from "./routes/userRoutes.js";
 import invoiceRoutes from "./routes/invoiceRoutes.js";
-import cors from 'cors'
+import cors from "cors";
 
 connectDB();
 
 const app = express();
 
-app.use(cors({
-    origin: 'https://invoice-withdb-bl7o-dev.fl0.io'
-}));
+app.use(
+  cors({
+    origin: "https://invoice-withdb-bl7o-dev.fl0.io",
+  })
+);
 
 app.use(express.json());
 
@@ -28,15 +30,15 @@ app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/invoices", invoiceRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname, "frontend/dist")));
-  
-    app.get("*", (req, res) =>
-      res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-    );
-  } else {
-    app.get("/", (req, res) => res.send("Server is ready bro."));
-  }
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => res.send("Server is ready bro."));
+}
 
 app.use(notFound);
 app.use(errorHandler);
