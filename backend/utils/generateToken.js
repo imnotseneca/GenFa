2058043bf338c6e16380e8970ac1da134cb7b051
BrunoC <corraobruno13@gmail.com>
2023-08-16 01:09:43 -0300

    removing console.log from generateToken to test token fixes

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -5,7 +5,6 @@ const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "30d",
   });
-  console.log(token)
 
   res.cookie("jwt", token, {
     httpOnly: true,
