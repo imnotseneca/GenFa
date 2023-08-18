2b5f6e624776f250172ee93176c4e782a0c88dff
BrunoC <corraobruno13@gmail.com>
2023-08-18 17:15:29 -0300

    fixed typo on the client url

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(
   cors({
-    origin:  "http://localhost:3000/",
+    origin:  "http://localhost:3000",
     methods: ["POST", "PUT", "GET", "DELETE"],
     credentials: true,
     exposedHeaders: ["set-cookie"],
