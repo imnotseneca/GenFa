a317b8a6553307da069d15612c33bff9138bbf19
BrunoC <corraobruno13@gmail.com>
2023-08-21 23:12:26 -0300

    modified cors options for vercel domain

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ connectDB();
 const app = express();
 
 const corsOptions = {
-    origin: 'https://invoice-withdb.vercel.app',
+    origin: 'https://genfa.vercel.app',
     credentials: true, // Important for sending cookies
   };
 
